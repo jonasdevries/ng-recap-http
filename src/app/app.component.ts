05f2c0e0038336423dfe53f5f87519ad847549a2
Jonas Devries <jonas@good-it.be>
2020-04-03 06:17:16 +0200

    show error with Subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
 import {Post} from './post.module';
 import {environment} from '../environments/environment';
 import {PostsService} from "./posts.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetching = false;
+  error = null;
+  private errorSub: Subscription;
 
   constructor(private http: HttpClient, private postService: PostsService) {}
 
+  ngOnDestroy(): void {
+    this.errorSub.unsubscribe();
+  }
+
   ngOnInit() {
+
+    this.errorSub = this.postService.error
+      .subscribe(errorMsg => {
+        this.error = errorMsg;
+     });
+
     this.isFetching = true;
     this.postService.fetchPosts()
       .subscribe(posts => {
         this.isFetching = false;
         this.loadedPosts = posts;
+      }, (error) => {
+        this.error = error.message;
       });
   }
 
@@ -35,7 +50,9 @@ export class AppComponent implements OnInit {
       .subscribe(posts => {
         this.isFetching = false;
         this.loadedPosts = posts;
-      });
+      }, (error => {
+        this.error = error.message;
+      }));
   }
 
   onClearPosts() {
