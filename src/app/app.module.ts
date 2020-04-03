46aa574098f36e720e80dd96576d18e7ca7e7a58
Jonas Devries <jonas@good-it.be>
2020-04-03 10:01:53 +0200

    HTTP Interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import {AuthInterceptService} from './auth-intercept.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptService,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
